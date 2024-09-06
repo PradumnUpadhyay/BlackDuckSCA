8d58c4f807346795adc95a27ffd7a02b8d062663
Pradumn Upadhyay <upradumn@zeomega.com>
2024-09-06 12:00:32 +0530

    changes to SA and BA

--- BDBA.ps1
+++ BDBA.ps1
@@ -15,4 +15,13 @@ $config=Read-Config
 $serverUrl=$config.server_url
 $token=$config.token
 
-[Net.ServicePointManager]::SecurityProtocol = 'tls12'; $Env:DETECT_EXIT_CODE_PASSTHRU=1; irm https://detect.synopsys.com/detect9.ps1?$(Get-Random) | iex; detect --blackduck.url="$serverUrl" --blackduck.api.token=$token --detect.project.name=APPSEC-2826 --detect.project.version.name=vault-1.6.2 --detect.source.path=D:\Pradumn\APPSEC-2826 --detect.binary.scan.file.path=D:\Pradumn\APPSEC-2826\vault_1.6.2_linux_amd64.zip --detect.tools.excluded=SIGNATURE_SCAN --detect.project.version.phase=PLANNING --detect.project.version.distribution=EXTERNAL
+[Net.ServicePointManager]::SecurityProtocol = 'tls12'; $Env:DETECT_EXIT_CODE_PASSTHRU=1; irm https://detect.synopsys.com/detect9.ps1?$(Get-Random) | iex; detect `
+ --blackduck.url="$serverUrl" `
+ --blackduck.api.token=$token `
+ --detect.project.name=APPSEC-2826 `
+ --detect.project.version.name=vault-1.6.2 `
+ --detect.source.path=D:\Pradumn\APPSEC-2826 `
+ --detect.binary.scan.file.path=D:\Pradumn\APPSEC-2826\vault_1.6.2_linux_amd64.zip `
+ --detect.tools.excluded=SIGNATURE_SCAN `
+ --detect.project.version.phase=PLANNING `
+ --detect.project.version.distribution=EXTERNAL
--- BDSA.ps1
+++ BDSA.ps1
@@ -15,4 +15,12 @@ $config=Read-Config
 $serverUrl=$config.server_url
 $token=$config.token
 
-[Net.ServicePointManager]::SecurityProtocol = 'tls12'; $Env:DETECT_EXIT_CODE_PASSTHRU=1; irm https://detect.synopsys.com/detect9.ps1?$(Get-Random) | iex; detect --blackduck.url="$serverUrl" --blackduck.api.token="$token" --detect.project.name=testing_7890 --detect.project.version.name=redhat --detect.blackduck.signature.scanner.paths=D:\Pradumn\APPSEC-2826\definitionservice-py3-v7.0.78+6.1.10.tar --detect.tools=CONTAINER_SCAN --detect.project.version.phase=PLANNING --detect.project.version.distribution=EXTERNAL
+[Net.ServicePointManager]::SecurityProtocol = 'tls12'; $Env:DETECT_EXIT_CODE_PASSTHRU=1; irm https://detect.synopsys.com/detect9.ps1?$(Get-Random) | iex; detect `
+ --blackduck.url="$serverUrl" `
+ --blackduck.api.token="$token" `
+ --detect.project.name=testing_7890 `
+ --detect.project.version.name=redhat `
+ --detect.blackduck.signature.scanner.paths=D:\Pradumn\APPSEC-2826\definitionservice-py3-v7.0.78+6.1.10.tar `
+ --detect.tools=CONTAINER_SCAN `
+ --detect.project.version.phase=PLANNING `
+ --detect.project.version.distribution=EXTERNAL
